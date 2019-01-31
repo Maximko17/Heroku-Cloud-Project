d238d2cc52421e7545a40d1c78ba59c7525c41b8
Maxim <maxim06122000@mail.ru>
2019-01-31 17:22:10 +0300

    add react in static folder

---
+++ src/main/resources/static/precache-manifest.d68b7ce4aa97ffbdf36a7b5aa82b0627.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0a8f4da3b18d59a71f7d",
+    "url": "/static/js/main.0a8f4da3.chunk.js"
+  },
+  {
+    "revision": "5167352d22846b606298",
+    "url": "/static/js/1.5167352d.chunk.js"
+  },
+  {
+    "revision": "0a8f4da3b18d59a71f7d",
+    "url": "/static/css/main.da4f5b7e.chunk.css"
+  },
+  {
+    "revision": "5167352d22846b606298",
+    "url": "/static/css/1.12084998.chunk.css"
+  },
+  {
+    "revision": "51e313f386dd8941d8e9fadf579c41e7",
+    "url": "/index.html"
+  }
+];
